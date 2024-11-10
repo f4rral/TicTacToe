5af62198b3e4d788c0b73e403c008ddc74ff08b0
f4rral <f4rral@gmail.com>
2024-11-10 16:12:09 +0300

    Добавлен dataBinding

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,6 +26,9 @@ android {
             )
         }
     }
+    buildFeatures {
+        dataBinding = true
+    }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
@@ -42,6 +45,7 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    implementation(libs.androidx.lifecycle.viewmodel.ktx)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- app/src/main/java/com/hfad/tictactoe/MainActivity.kt
+++ app/src/main/java/com/hfad/tictactoe/MainActivity.kt
@@ -2,10 +2,17 @@ package com.hfad.tictactoe
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.hfad.tictactoe.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private var _binding: ActivityMainBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        _binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.msg = "Hello dataBinding!"
     }
 }
