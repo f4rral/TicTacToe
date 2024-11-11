3b407b8df7ca601ef78e038267ace3889a14c4f3
f4rral <f4rral@gmail.com>
2024-11-11 21:08:36 +0300

    Связь MainActivity c макетом

--- app/src/main/java/com/hfad/tictactoe/MainActivity.kt
+++ app/src/main/java/com/hfad/tictactoe/MainActivity.kt
@@ -3,16 +3,21 @@ package com.hfad.tictactoe
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import com.hfad.tictactoe.viewmodels.GameViewModel
 import com.hfad.tictactoe.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private var _binding: ActivityMainBinding? = null
     private val binding get() = _binding!!
+    lateinit var viewModel: GameViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         _binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
-        binding.msg = "Hello dataBinding!"
+
+        viewModel = GameViewModel()
+
+        binding.viewModel = viewModel
+        binding.setLifecycleOwner(this)
     }
 }
